ce40c0d0b9de94c0018979dfb815d734262ba1d2
Devin Chasanoff <devchas@google.com>
2019-04-11 16:03:35 -0400

    Fix test namespace

--- google-ads-migration-examples/src/test/java/com.google.ads.googleads.migration.utils/CodeSampleParamsTest.java
+++ google-ads-migration-examples/src/test/java/com.google.ads.googleads.migration.utils/CodeSampleParamsTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.ads.googleads.examples.utils;
+package com.google.ads.googleads.migration.utils;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
