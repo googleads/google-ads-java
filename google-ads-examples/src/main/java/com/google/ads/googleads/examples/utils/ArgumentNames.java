871d2fcdf4c4e661c7b9f35115e9b1ac11e546a1
Devin Chasanoff <devchas@google.com>
2019-05-24 14:07:59 -0400

    Start GMB location ext example

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/utils/ArgumentNames.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/utils/ArgumentNames.java
@@ -38,4 +38,7 @@ public final class ArgumentNames {
   public static final String REPLACE_EXISTING_TREE = "--replaceExistingTree";
   public static final String PAGE_URL = "--pageUrl";
   public static final String LABEL_ID = "--labelId";
+  public static final String GMB_EMAIL_ADDRESS = "--gmbEmailAddress";
+  public static final String BUSINESS_ACCOUNT_IDENTIFIER = "--businessAccountIdentifier";
+  public static final String GMB_ACCESS_TOKEN = "--gmbAccessToken";
 }
