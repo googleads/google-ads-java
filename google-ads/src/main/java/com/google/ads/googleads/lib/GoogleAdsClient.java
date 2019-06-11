693a66fb0dce5efcfc08ad6d5a70e4986059e738
Nick Birnie <nbirnie@google.com>
2019-06-11 11:19:04 +0100

    Set max response message size to 64MB (#136)

--- google-ads/src/main/java/com/google/ads/googleads/lib/GoogleAdsClient.java
+++ google-ads/src/main/java/com/google/ads/googleads/lib/GoogleAdsClient.java
@@ -79,6 +79,9 @@ public abstract class GoogleAdsClient extends AbstractGoogleAdsClient {
             // Issue 131: inbound headers may exceed default (8kb) max header size.
             // Sets max header size to 16MB, which should be more than necessary.
             .setMaxInboundMetadataSize(16 * 1024 * 1024)
+            // Sets max response size to 64MB, since large responses will often exceed the default
+            // (4MB).
+            .setMaxInboundMessageSize(64 * 1024 * 1024)
             .build();
     clientBuilder
         .setEndpoint(DEFAULT_ENDPOINT)
