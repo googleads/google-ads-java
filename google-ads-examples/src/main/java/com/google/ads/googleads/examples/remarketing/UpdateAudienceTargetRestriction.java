6337b9e11823bf0e947da133adf6aafe8ebb189d
devchas <devchas@google.com>
2020-03-10 11:12:22 -0400

    Address Knack comments

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/remarketing/UpdateAudienceTargetRestriction.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/remarketing/UpdateAudienceTargetRestriction.java
@@ -35,16 +35,19 @@ import com.google.ads.googleads.v3.services.SearchGoogleAdsRequest;
 import com.google.ads.googleads.v3.utils.ResourceNames;
 import com.google.common.collect.ImmutableList;
 import com.google.protobuf.BoolValue;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
-/** Sets the targeting setting of an ad group to bid only for targeted audiences. */
-public class UpdateTargetingSetting {
+/**
+ * Updates the AUDIENCE target restriction of a given ad group to bid only.
+ */
+public class UpdateAudienceTargetRestriction {
 
   private static final int PAGE_SIZE = 1_000;
 
-  private static class UpdateTargetingSettingParams extends CodeSampleParams {
+  private static class UpdateAudienceTargetRestrictionParams extends CodeSampleParams {
 
     @Parameter(names = ArgumentNames.CUSTOMER_ID, required = true)
     private Long customerId;
@@ -54,7 +57,7 @@ public class UpdateTargetingSetting {
   }
 
   public static void main(String[] args) {
-    UpdateTargetingSettingParams params = new UpdateTargetingSettingParams();
+    UpdateAudienceTargetRestrictionParams params = new UpdateAudienceTargetRestrictionParams();
     if (!params.parseArguments(args)) {
 
       // Either pass the required parameters for this example on the command line, or insert them
@@ -68,7 +71,7 @@ public class UpdateTargetingSetting {
       googleAdsClient = GoogleAdsClient.newBuilder().fromPropertiesFile().build();
     } catch (FileNotFoundException fnfe) {
       System.err.printf(
-          "Failed to load GoogleAdsClient configuration from file. Exception: %s%n", fnfe);
+        "Failed to load GoogleAdsClient configuration from file. Exception: %s%n", fnfe);
       return;
     } catch (IOException ioe) {
       System.err.printf("Failed to create GoogleAdsClient. Exception: %s%n", ioe);
@@ -76,15 +79,18 @@ public class UpdateTargetingSetting {
     }
 
     try {
-      new UpdateTargetingSetting().runExample(googleAdsClient, params.customerId, params.adGroupId);
+      new UpdateAudienceTargetRestriction().runExample(
+        googleAdsClient,
+        params.customerId,
+        params.adGroupId);
     } catch (GoogleAdsException gae) {
       // GoogleAdsException is the base class for most exceptions thrown by an API request.
       // Instances of this exception have a message and a GoogleAdsFailure that contains a
       // collection of GoogleAdsErrors that indicate the underlying causes of the
       // GoogleAdsException.
       System.err.printf(
-          "Request ID %s failed due to GoogleAdsException. Underlying errors:%n",
-          gae.getRequestId());
+        "Request ID %s failed due to GoogleAdsException. Underlying errors:%n",
+        gae.getRequestId());
       int i = 0;
       for (GoogleAdsError googleAdsError : gae.getGoogleAdsFailure().getErrorsList()) {
         System.err.printf("  Error %d: %s%n", i++, googleAdsError);
@@ -103,23 +109,27 @@ public class UpdateTargetingSetting {
   private void runExample(GoogleAdsClient googleAdsClient, long customerId, long adGroupId) {
     // Creates an empty TargetingSetting object.
     TargetingSetting.Builder targetingSettingBuilder = TargetingSetting.newBuilder();
+    // Creates a flag that specifies whether or not we should update the targeting setting. We
+    // should only do this if we find an AUDIENCE target restriction with bid_only set to false.
+    boolean shouldUpdateTargetingSetting = false;
 
     // Creates the Google Ads service client.
     try (GoogleAdsServiceClient googleAdsServiceClient =
-        googleAdsClient.getLatestVersion().createGoogleAdsServiceClient()) {
+           googleAdsClient.getLatestVersion().createGoogleAdsServiceClient()) {
       // Creates a search request that retrieves the targeting settings from a given ad group.
       String searchQuery =
-          "SELECT ad_group.id, ad_group.name, ad_group.targeting_setting.target_restrictions FROM"
-              + " ad_group WHERE ad_group.id = "
-              + adGroupId;
+        "SELECT ad_group.id, ad_group.name, ad_group.targeting_setting.target_restrictions " +
+          "FROM ad_group " +
+          "WHERE ad_group.id = "
+          + adGroupId;
 
       // Creates a request that will retrieve all ad groups using pages of the specified page size.
       SearchGoogleAdsRequest request =
-          SearchGoogleAdsRequest.newBuilder()
-              .setCustomerId(Long.toString(customerId))
-              .setPageSize(PAGE_SIZE)
-              .setQuery(searchQuery)
-              .build();
+        SearchGoogleAdsRequest.newBuilder()
+          .setCustomerId(Long.toString(customerId))
+          .setPageSize(PAGE_SIZE)
+          .setQuery(searchQuery)
+          .build();
       // Issues the search request.
       SearchPagedResponse searchPagedResponse = googleAdsServiceClient.search(request);
       // Iterates over all rows in all pages and prints the requested field values for the ad group
@@ -128,11 +138,11 @@ public class UpdateTargetingSetting {
         AdGroup adGroup = googleAdsRow.getAdGroup();
         // Prints the results.
         System.out.printf(
-            "Ad group with ID %d and name '%s' was found with the following targeting"
-                + " restrictions.%n",
-            adGroup.getId().getValue(), adGroup.getName().getValue());
+          "Ad group with ID %d and name '%s' was found with the following targeting"
+            + " restrictions.%n",
+          adGroup.getId().getValue(), adGroup.getName().getValue());
         List<TargetRestriction> targetRestrictions =
-            adGroup.getTargetingSetting().getTargetRestrictionsList();
+          adGroup.getTargetingSetting().getTargetRestrictionsList();
         // Loops through and prints each of the target restrictions.
         // Reconstructs the TargetingSetting object with the updated audience target restriction
         // because Google will overwrite the entire targeting_setting field of the ad group when
@@ -141,51 +151,78 @@ public class UpdateTargetingSetting {
           TargetingDimension targetingDimension = targetRestriction.getTargetingDimension();
           boolean bidOnly = targetRestriction.getBidOnly().getValue();
           System.out.printf(
-              "  Targeting restriction with targeting dimension '%s' and bid only set to '%b'.%n",
-              targetingDimension, bidOnly);
+            "- Targeting restriction with targeting dimension '%s' and bid only set to '%b'.%n",
+            targetingDimension, bidOnly);
           // Adds the target restriction to the TargetingSetting object as is if the targeting
           // dimension has a value other than AUDIENCE because those should not change.
           if (!targetingDimension.equals(TargetingDimension.AUDIENCE)) {
             targetingSettingBuilder.addTargetRestrictions(targetRestriction);
-            // Adds the target restriction to the TargetingSetting object with bid_only set to true
-            // if the targeting dimension equals AUDIENCE in order to update that target
-            // restriction to "Observation".
-            // For more details about the targeting setting, visit
+          } else if (!bidOnly) {
+            shouldUpdateTargetingSetting = true;
+            // Adds an AUDIENCE target restriction with bid_only set to true to the targeting
+            // setting object. This has the effect of setting the AUDIENCE target restriction to
+            // "Observation". For more details about the targeting setting, visit
             // https://support.google.com/google-ads/answer/7365594.
-          } else {
             targetingSettingBuilder.addTargetRestrictions(
-                TargetRestriction.newBuilder()
-                    .setTargetingDimensionValue(TargetingDimension.AUDIENCE_VALUE)
-                    .setBidOnly(BoolValue.of(true)));
+              TargetRestriction.newBuilder()
+                .setTargetingDimensionValue(TargetingDimension.AUDIENCE_VALUE)
+                .setBidOnly(BoolValue.of(true)));
           }
         }
       }
     }
 
+    // Only updates the TargetingSetting on the ad group if there is an AUDIENCE TargetRestriction
+    // with  bid_only set to false.
+    if (shouldUpdateTargetingSetting) {
+      updateTargetingSetting(googleAdsClient,
+        customerId,
+        adGroupId,
+        targetingSettingBuilder.build());
+    } else {
+      System.out.println("No target restrictions to update.");
+    }
+  }
+
+  /**
+   * Updates the TargetingSetting of an ad group.
+   *
+   * @param googleAdsClient the Google Ads API client.
+   * @param customerId the client customer ID.
+   * @param adGroupId the ID of the ad group to update.
+   * @param targetingSetting the updated targeting setting.
+   */
+  private void updateTargetingSetting(
+    GoogleAdsClient googleAdsClient,
+    long customerId,
+    long adGroupId,
+    TargetingSetting targetingSetting) {
     // Creates the ad group service client.
     try (AdGroupServiceClient adGroupServiceClient =
-        googleAdsClient.getLatestVersion().createAdGroupServiceClient()) {
+           googleAdsClient.getLatestVersion().createAdGroupServiceClient()) {
       // Creates an ad group object with the proper resource name and updated targeting setting.
       AdGroup adGroup =
-          AdGroup.newBuilder()
-              .setResourceName(ResourceNames.adGroup(customerId, adGroupId))
-              .setTargetingSetting(targetingSettingBuilder.build())
-              .build();
+        AdGroup.newBuilder()
+          .setResourceName(ResourceNames.adGroup(customerId, adGroupId))
+          .setTargetingSetting(targetingSetting)
+          .build();
       // Constructs an operation that will update the ad group, using the FieldMasks utility to
       // derive the update mask. This mask tells the Google Ads API which attributes of the
       // ad group you want to change.
       AdGroupOperation operation =
-          AdGroupOperation.newBuilder()
-              .setUpdate(adGroup)
-              .setUpdateMask(FieldMasks.allSetFieldsOf(adGroup))
-              .build();
+        AdGroupOperation.newBuilder()
+          .setUpdate(adGroup)
+          .setUpdateMask(FieldMasks.allSetFieldsOf(adGroup))
+          .build();
       // Sends the operation in a mutate request.
       MutateAdGroupsResponse response =
-          adGroupServiceClient.mutateAdGroups(
-              Long.toString(customerId), ImmutableList.of(operation));
+        adGroupServiceClient.mutateAdGroups(
+          Long.toString(customerId), ImmutableList.of(operation));
       // Prints the resource name of the updated object.
       System.out.printf(
-          "Updated ad group with resourceName: '%s'.%n", response.getResults(0).getResourceName());
+        "Updated targeting setting of ad group with resourceName '%s'; set the AUDIENCE " +
+          "target restriction to bid_only.%n",
+        response.getResults(0).getResourceName());
     }
   }
 }
