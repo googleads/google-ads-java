0e44a558e67bdbee7913835675ea326bef57d87d
Nick Birnie <nbirnie@google.com>
2020-04-28 19:12:52 +0100

    Address review comments
    Change-Id: I6b91940260f6ea01f9a3eb7f7b9ec55062cf1ad7

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/basicoperations/UpdateExpandedTextAd.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/basicoperations/UpdateExpandedTextAd.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ import com.google.protobuf.StringValue;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-/** Updates an expanded text ad. To get expanded text ads, run GetExpandedTextAds.cs. */
+/** Updates an expanded text ad. To get expanded text ads, run GetExpandedTextAds. */
 public class UpdateExpandedTextAd {
 
   private static class UpdateExpandedTextAdParams extends CodeSampleParams {
@@ -50,6 +50,7 @@ public class UpdateExpandedTextAd {
       // Either pass the required parameters for this example on the command line, or insert them
       // into the code here. See the parameter class definition above for descriptions.
       params.customerId = Long.parseLong("INSERT_CUSTOMER_ID_HERE");
+      params.adId = Long.parseLong("INSERT_AD_ID_HERE");
     }
 
     GoogleAdsClient googleAdsClient;
@@ -81,7 +82,13 @@ public class UpdateExpandedTextAd {
     }
   }
 
-  /** Runs the example. */
+  /**
+   * Runs the example.
+   *
+   * @param googleAdsClient the client to use for connecting to the API.
+   * @param customerId the customer ID to update.
+   * @param adId the ad ID to update.
+   */
   private void runExample(GoogleAdsClient googleAdsClient, long customerId, long adId) {
     // Creates an AdOperation to update an ad.
     AdOperation.Builder adOperation = AdOperation.newBuilder();
