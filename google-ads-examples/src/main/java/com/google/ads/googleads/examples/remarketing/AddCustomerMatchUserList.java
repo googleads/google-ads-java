9f6c0bb4f9ccf28bf00f040d5125d265339748fd
devchas <devchas@google.com>
2020-10-20 08:58:23 -0400

    Update messaging in AddCustomerMatchUserList example
    Change-Id: I9cec5b2a44d3bb4808f552af19fe714a0f382eb7

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/remarketing/AddCustomerMatchUserList.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/remarketing/AddCustomerMatchUserList.java
@@ -256,6 +256,8 @@ public class AddCustomerMatchUserList {
       System.out.println("Waiting until operation completes.");
 
       // The polling future implements a default back-off policy for retrying.
+      // Note: Retrieving the results of the asynchronous request is for demonstrative purposes only
+      // as it may take several hours for the user list to be populated with the users.
       runFuture.getPollingFuture().get(MAX_TOTAL_POLL_INTERVAL_SECONDS, TimeUnit.SECONDS);
       System.out.printf(
           "Offline user data job with resource name '%s' has finished.%n",
