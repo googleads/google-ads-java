85087916dbfd7a6e2ae718f93968efb1598b0825
pierrick <pierrick@google.com>
2020-12-10 16:15:47 -0500

    Reverted one code example (out of scope)

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/basicoperations/PauseAd.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/basicoperations/PauseAd.java
@@ -14,21 +14,38 @@
 
 package com.google.ads.googleads.examples.basicoperations;
 
+import com.beust.jcommander.Parameter;
+import com.google.ads.googleads.examples.utils.ArgumentNames;
 import com.google.ads.googleads.examples.utils.CodeSampleParams;
 import com.google.ads.googleads.lib.GoogleAdsClient;
 import com.google.ads.googleads.lib.utils.FieldMasks;
-import com.google.ads.googleads.v6.common.ManualCpc;
+import com.google.ads.googleads.v6.enums.AdGroupAdStatusEnum.AdGroupAdStatus;
 import com.google.ads.googleads.v6.errors.GoogleAdsError;
 import com.google.ads.googleads.v6.errors.GoogleAdsException;
-import com.google.ads.googleads.v6.resources.Campaign;
-import com.google.protobuf.FieldMask;
+import com.google.ads.googleads.v6.resources.AdGroupAd;
+import com.google.ads.googleads.v6.services.AdGroupAdOperation;
+import com.google.ads.googleads.v6.services.AdGroupAdServiceClient;
+import com.google.ads.googleads.v6.services.MutateAdGroupAdResult;
+import com.google.ads.googleads.v6.services.MutateAdGroupAdsResponse;
+import com.google.ads.googleads.v6.utils.ResourceNames;
+import com.google.common.collect.ImmutableList;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /** Changes the status of a given ad to {@code PAUSED}. */
 public class PauseAd {
 
-  private static class PauseAdParams extends CodeSampleParams {}
+  private static class PauseAdParams extends CodeSampleParams {
+
+    @Parameter(names = ArgumentNames.CUSTOMER_ID, required = true)
+    private Long customerId;
+
+    @Parameter(names = ArgumentNames.AD_GROUP_ID, required = true)
+    private Long adGroupId;
+
+    @Parameter(names = ArgumentNames.AD_ID, required = true)
+    private Long adId;
+  }
 
   public static void main(String[] args) {
     PauseAdParams params = new PauseAdParams();
@@ -36,6 +53,9 @@ public class PauseAd {
 
       // Either pass the required parameters for this example on the command line, or insert them
       // into the code here. See the parameter class definition above for descriptions.
+      params.customerId = Long.parseLong("INSERT_CUSTOMER_ID_HERE");
+      params.adGroupId = Long.parseLong("INSERT_AD_GROUP_ID_HERE");
+      params.adId = Long.parseLong("INSERT_AD_ID_HERE");
     }
 
     GoogleAdsClient googleAdsClient = null;
@@ -51,7 +71,7 @@ public class PauseAd {
     }
 
     try {
-      new PauseAd().runExample(googleAdsClient);
+      new PauseAd().runExample(googleAdsClient, params.customerId, params.adGroupId, params.adId);
     } catch (GoogleAdsException gae) {
       // GoogleAdsException is the base class for most exceptions thrown by an API request.
       // Instances of this exception have a message and a GoogleAdsFailure that contains a
@@ -72,40 +92,36 @@ public class PauseAd {
    * Runs the example.
    *
    * @param googleAdsClient the Google Ads API client.
+   * @param customerId the client customer ID.
+   * @param adGroupId the ad group ID.
+   * @param adId the ID of the ad to pause.
    * @throws GoogleAdsException if an API request failed with one or more service errors.
    */
-  private void runExample(GoogleAdsClient googleAdsClient) {
+  private void runExample(
+      GoogleAdsClient googleAdsClient, long customerId, long adGroupId, long adId) {
 
-    // String adGroupAdResourceName = ResourceNames.adGroupAd(customerId, adGroupId, adId);
-    //
-    // // Creates an ad representation with its status set to PAUSED.
-    // AdGroupAd adGroupAd =
-    //     AdGroupAd.newBuilder()
-    //         .setResourceName(adGroupAdResourceName)
-    //         .setStatus(AdGroupAdStatus.PAUSED)
-    //         .build();
-    //
-    // AdGroupAdOperation op =
-    //     AdGroupAdOperation.newBuilder()
-    //         .setUpdate(adGroupAd)
-    //         .setUpdateMask(FieldMasks.allSetFieldsOf(adGroupAd))
-    //         .build();
-    //
-    // try (AdGroupAdServiceClient adGroupAdServiceClient =
-    //     googleAdsClient.getLatestVersion().createAdGroupAdServiceClient()) {
-    //   MutateAdGroupAdsResponse response =
-    //       adGroupAdServiceClient.mutateAdGroupAds(Long.toString(customerId),
-    // ImmutableList.of(op));
-    //   for (MutateAdGroupAdResult result : response.getResultsList()) {
-    //     System.out.printf("Ad with resource name '%s' is paused.%n", result.getResourceName());
-    //   }
-    // }
+    String adGroupAdResourceName = ResourceNames.adGroupAd(customerId, adGroupId, adId);
 
-    Campaign campaign =
-        Campaign.newBuilder()
-            .setManualCpc(ManualCpc.newBuilder().setEnhancedCpcEnabled(false).build())
+    // Creates an ad representation with its status set to PAUSED.
+    AdGroupAd adGroupAd =
+        AdGroupAd.newBuilder()
+            .setResourceName(adGroupAdResourceName)
+            .setStatus(AdGroupAdStatus.PAUSED)
             .build();
-    FieldMask fieldMask = FieldMasks.allSetFieldsOf(campaign);
-    System.out.println(fieldMask.toString());
+
+    AdGroupAdOperation op =
+        AdGroupAdOperation.newBuilder()
+            .setUpdate(adGroupAd)
+            .setUpdateMask(FieldMasks.allSetFieldsOf(adGroupAd))
+            .build();
+
+    try (AdGroupAdServiceClient adGroupAdServiceClient =
+        googleAdsClient.getLatestVersion().createAdGroupAdServiceClient()) {
+      MutateAdGroupAdsResponse response =
+          adGroupAdServiceClient.mutateAdGroupAds(Long.toString(customerId), ImmutableList.of(op));
+      for (MutateAdGroupAdResult result : response.getResultsList()) {
+        System.out.printf("Ad with resource name '%s' is paused.%n", result.getResourceName());
+      }
+    }
   }
 }
