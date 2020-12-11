43cb5706752e435f6d7950c08cf8b66b36d35ba8
Nick Birnie <nbirnie@google.com>
2020-12-11 17:48:17 +0000

    Fix campaign draft (#391)

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/campaignmanagement/AddCampaignDraft.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/campaignmanagement/AddCampaignDraft.java
@@ -41,7 +41,7 @@ public class AddCampaignDraft {
     @Parameter(names = ArgumentNames.CUSTOMER_ID, required = true)
     private Long customerId;
 
-    @Parameter(names = ArgumentNames.CAMPAIGN_ID, required = true)
+    @Parameter(names = ArgumentNames.BASE_CAMPAIGN_ID, required = true)
     private Long baseCampaignId;
   }
 
@@ -52,7 +52,7 @@ public class AddCampaignDraft {
       // Either pass the required parameters for this example on the command line, or insert them
       // into the code here. See the parameter class definition above for descriptions.
       params.customerId = Long.parseLong("INSERT_CUSTOMER_ID_HERE");
-      params.baseCampaignId = Long.valueOf("INSERT_CAMPAIGN_ID_HERE");
+      params.baseCampaignId = Long.valueOf("INSERT_BASE_CAMPAIGN_ID_HERE");
     }
 
     GoogleAdsClient googleAdsClient;
