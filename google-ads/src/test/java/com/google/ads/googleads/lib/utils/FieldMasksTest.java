40c384dd3b3509379422638bb9780e57c800196b
Devin Chasanoff <devchas@google.com>
2020-12-16 12:00:09 -0500

    add getFieldValue method to FieldMasks (#387)

--- google-ads/src/test/java/com/google/ads/googleads/lib/utils/FieldMasksTest.java
+++ google-ads/src/test/java/com/google/ads/googleads/lib/utils/FieldMasksTest.java
@@ -1,24 +1,134 @@
 package com.google.ads.googleads.lib.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-import com.google.ads.googleads.v5.resources.Campaign;
+import com.google.ads.googleads.v6.common.ManualCpc;
+import com.google.ads.googleads.v6.enums.CampaignStatusEnum.CampaignStatus;
+import com.google.ads.googleads.v6.resources.Campaign;
 import com.google.common.collect.ImmutableList;
+import com.google.protobuf.Descriptors.EnumValueDescriptor;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class FieldMasksTest {
+  private Campaign campaign;
+
+  @Before
+  public void setUp() {
+    Campaign.Builder builder =
+        Campaign.newBuilder().setName("New campaign").setStatus(CampaignStatus.PAUSED);
+    builder.getManualCpcBuilder().setEnhancedCpcEnabled(false);
+    campaign = builder.build();
+  }
 
   // Regression test for https://github.com/googleads/google-ads-java/issues/344.
   @Test
   public void optionalField_withDefaultValue_detectsChange() {
     Campaign.Builder builder = Campaign.newBuilder();
     builder.getManualCpcBuilder().setEnhancedCpcEnabled(false);
-    Campaign campaign = builder.build();
+    campaign = builder.build();
     assertEquals(
         ImmutableList.of("manual_cpc.enhanced_cpc_enabled"),
         FieldMasks.allSetFieldsOf(campaign).getPathsList());
+  }
+
+  @Test
+  public void getFieldValue_gets_basic_field() {
+    assertEquals(FieldMasks.getFieldValue("name", campaign), campaign.getName());
+  }
+
+  @Test
+  public void getFieldValue_assigns_basic_field() {
+    String name = FieldMasks.getFieldValue("name", campaign);
+    Campaign campaignToCompare = campaign.toBuilder().setName(name).build();
+    assertEquals(campaign, campaignToCompare);
+  }
+
+  @Test
+  public void getFieldValue_gets_nested_field() {
+    assertEquals(
+        FieldMasks.getFieldValue("manual_cpc.enhanced_cpc_enabled", campaign),
+        campaign.getManualCpc().getEnhancedCpcEnabled());
+  }
+
+  @Test
+  public void getFieldValue_assigns_nested_field() {
+    boolean enhancedCpcEnabled =
+        FieldMasks.getFieldValue("manual_cpc.enhanced_cpc_enabled", campaign);
+    Campaign.Builder campaignToCompare = campaign.toBuilder();
+    campaignToCompare.getManualCpcBuilder().setEnhancedCpcEnabled(enhancedCpcEnabled);
+    assertEquals(campaign, campaignToCompare.build());
+  }
+
+  @Test
+  public void getFieldValue_gets_top_nested_field() {
+    assertEquals(FieldMasks.getFieldValue("manual_cpc", campaign), campaign.getManualCpc());
+  }
+
+  @Test
+  public void getFieldValue_assigns_top_nested_field() {
+    ManualCpc manualCpc = FieldMasks.getFieldValue("manual_cpc", campaign);
+    Campaign campaignToCompare = campaign.toBuilder().setManualCpc(manualCpc).build();
+    assertEquals(campaign, campaignToCompare);
+  }
+
+  @Test
+  public void getFieldValue_gets_enum_field() {
+    assertEquals(
+        FieldMasks.getFieldValue("status", campaign), campaign.getStatus().getValueDescriptor());
+  }
+
+  @Test
+  public void getFieldValue_assigns_enum_value() {
+    EnumValueDescriptor status = FieldMasks.getFieldValue("status", campaign);
+    Campaign.Builder campaignToCompare = campaign.toBuilder();
+    campaignToCompare.setStatus(CampaignStatus.forNumber(status.getNumber()));
+    assertEquals(campaign, campaignToCompare.build());
+  }
+
+  @Test
+  public void getFieldValue_no_match_throws_exception() {
+    IllegalArgumentException exception =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+              FieldMasks.getFieldValue("foo.bar", campaign);
+            });
+    assertEquals(
+        exception.getMessage(),
+        "Cannot retrieve field value. A matching field was not found after navigating foo.bar up to"
+            + " foo");
+  }
+
+  @Test
+  public void getFieldValue_repeated_throws_exception() {
+    IllegalArgumentException exception =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+              FieldMasks.getFieldValue("url_custom_parameters.key", campaign);
+            });
+    assertEquals(
+        exception.getMessage(),
+        "Cannot access repeated sub-field url_custom_parameters in path url_custom_parameters.key"
+            + " of class com.google.ads.googleads.v6.resources.Campaign");
+  }
+
+  @Test
+  public void getFieldValue_non_message_throws_exception() {
+    IllegalArgumentException exception =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+              FieldMasks.getFieldValue("status.paused", campaign);
+            });
+    assertEquals(
+        exception.getMessage(),
+        "Cannot access a field nested inside a primitive or enum status in path status.paused in"
+            + " class com.google.ads.googleads.v6.resources.Campaign");
   }
 }
