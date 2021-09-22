9181b9053c6d3522dddf8deb29f6afb74d3c599f
Josh Radcliff <jradcliff@users.noreply.github.com>
2021-09-22 12:19:05 -0400

    Adds examples for conversion enhancements and form data uploads.
    Also adds an `--orderId` parameter to UploadOfflineConversion
    since order ID is required for enhancements.

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/remarketing/UploadConversionWithIdentifiers.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/remarketing/UploadConversionWithIdentifiers.java
@@ -1,4 +1,4 @@
-// Copyright 2020 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,21 +18,25 @@ import com.beust.jcommander.Parameter;
 import com.google.ads.googleads.examples.utils.ArgumentNames;
 import com.google.ads.googleads.examples.utils.CodeSampleParams;
 import com.google.ads.googleads.lib.GoogleAdsClient;
+import com.google.ads.googleads.v8.common.UserIdentifier;
+import com.google.ads.googleads.v8.enums.UserIdentifierSourceEnum.UserIdentifierSource;
 import com.google.ads.googleads.v8.errors.GoogleAdsError;
 import com.google.ads.googleads.v8.errors.GoogleAdsException;
 import com.google.ads.googleads.v8.services.ClickConversion;
 import com.google.ads.googleads.v8.services.ClickConversionResult;
 import com.google.ads.googleads.v8.services.ConversionUploadServiceClient;
-import com.google.ads.googleads.v8.services.CustomVariable;
 import com.google.ads.googleads.v8.services.UploadClickConversionsRequest;
 import com.google.ads.googleads.v8.services.UploadClickConversionsResponse;
 import com.google.ads.googleads.v8.utils.ResourceNames;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-/** Imports offline conversion values for specific clicks to an account. */
-public class UploadOfflineConversion {
-  private static class UploadOfflineConversionParams extends CodeSampleParams {
+/** Uploads a conversion using hashed email address instead of GCLID. */
+public class UploadConversionWithIdentifiers {
+  private static class UploadConversionWithIdentifiersParams extends CodeSampleParams {
 
     @Parameter(names = ArgumentNames.CUSTOMER_ID, required = true)
     private long customerId;
@@ -40,8 +44,11 @@ public class UploadOfflineConversion {
     @Parameter(names = ArgumentNames.CONVERSION_ACTION_ID, required = true)
     private long conversionActionId;
 
-    @Parameter(names = ArgumentNames.GCLID, required = true)
-    private String gclid;
+    @Parameter(names = ArgumentNames.EMAIL_ADDRESS, required = true)
+    private String emailAddress;
+
+    @Parameter(names = ArgumentNames.ORDER_ID, required = false)
+    private String orderId;
 
     @Parameter(
         names = ArgumentNames.CONVERSION_DATE_TIME,
@@ -49,41 +56,26 @@ public class UploadOfflineConversion {
         description =
             "The date time at which the conversion occurred. "
                 + "Must be after the click time, and must include the time zone offset. "
-                + "The format is  'yyyy-mm-dd hh:mm:ss+|-hh:mm', e.g. '2019-01-01 12:32:45-08:00'.")
+                + "The format is 'yyyy-mm-dd hh:mm:ss+|-hh:mm', e.g. '2019-01-01 12:32:45-08:00'.")
     private String conversionDateTime;
 
     @Parameter(names = ArgumentNames.CONVERSION_VALUE, required = true)
     private Double conversionValue;
-
-    // Optional: Specify the conversion custom variable ID and value you want to
-    // associate with the click conversion upload.
-    @Parameter(names = ArgumentNames.CONVERSION_CUSTOM_VARIABLE_ID)
-    private Long conversionCustomVariableId;
-
-    @Parameter(names = ArgumentNames.CONVERSION_CUSTOM_VARIABLE_VALUE)
-    private String conversionCustomVariableValue;
-
-    // Optional: Specify the unique order ID for the click conversion.
-    @Parameter(names = ArgumentNames.ORDER_ID)
-    private String orderId;
   }
 
-  public static void main(String[] args) {
-    UploadOfflineConversionParams params = new UploadOfflineConversionParams();
+  public static void main(String[] args)
+      throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    UploadConversionWithIdentifiersParams params = new UploadConversionWithIdentifiersParams();
     if (!params.parseArguments(args)) {
 
       // Either pass the required parameters for this example on the command line, or insert them
       // into the code here. See the parameter class definition above for descriptions.
       params.customerId = Long.parseLong("INSERT_CUSTOMER_ID_HERE");
       params.conversionActionId = Long.parseLong("INSERT_CONVERSION_ACTION_ID_HERE");
-      params.gclid = "INSERT_GCL_ID_HERE";
+      params.emailAddress = "INSERT_EMAIL_ADDRESS_HERE";
       params.conversionDateTime = "INSERT_CONVERSION_DATE_TIME_HERE";
       params.conversionValue = Double.parseDouble("INSERT_CONVERSION_VALUE_HERE");
-      // Optionally specify the conversion custom variable ID and value you want to
-      // associate with the click conversion upload.
-      params.conversionCustomVariableId = null;
-      params.conversionCustomVariableValue = null;
-      // Optionally specify the order ID for the click conversion.
+      // Optional: Specify the unique order ID for the click conversion.
       params.orderId = null;
     }
 
@@ -100,17 +92,15 @@ public class UploadOfflineConversion {
     }
 
     try {
-      new UploadOfflineConversion()
+      new UploadConversionWithIdentifiers()
           .runExample(
               googleAdsClient,
               params.customerId,
               params.conversionActionId,
-              params.gclid,
+              params.emailAddress,
+              params.orderId,
               params.conversionDateTime,
-              params.conversionValue,
-              params.conversionCustomVariableId,
-              params.conversionCustomVariableValue,
-              params.orderId);
+              params.conversionValue);
     } catch (GoogleAdsException gae) {
       // GoogleAdsException is the base class for most exceptions thrown by an API request.
       // Instances of this exception have a message and a GoogleAdsFailure that contains a
@@ -133,55 +123,60 @@ public class UploadOfflineConversion {
    * @param googleAdsClient the Google Ads API client.
    * @param customerId the client customer ID.
    * @param conversionActionId conversion action ID associated with this conversion.
-   * @param gclid the GCLID for the conversion.
+   * @param emailAddress email address for the conversion.
+   * @param orderId the unique ID (transaction ID) of the conversion.
    * @param conversionDateTime date and time of the conversion.
    * @param conversionValue the value of the conversion.
-   * @param conversionCustomVariableId the ID of the conversion custom variable to associate with
-   *     the upload.
-   * @param conversionCustomVariableValue the value of the conversion custom variable to associate
-   *     with the upload.
-   * @param orderId the unique ID (transaction ID) of the conversion.
    */
-  // [START upload_offline_conversion]
+  // [START upload_conversion_with_identifiers]
   private void runExample(
       GoogleAdsClient googleAdsClient,
       long customerId,
       long conversionActionId,
-      String gclid,
+      String emailAddress,
+      String orderId,
       String conversionDateTime,
-      Double conversionValue,
-      Long conversionCustomVariableId,
-      String conversionCustomVariableValue,
-      String orderId) {
+      Double conversionValue)
+      throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    // [START create_conversion]
     // Gets the conversion action resource name.
     String conversionActionResourceName =
         ResourceNames.conversionAction(customerId, conversionActionId);
 
-    // Creates the click conversion.
+    // Creates a builder for constructing the click conversion.
     ClickConversion.Builder clickConversionBuilder =
         ClickConversion.newBuilder()
             .setConversionAction(conversionActionResourceName)
             .setConversionDateTime(conversionDateTime)
             .setConversionValue(conversionValue)
-            .setCurrencyCode("USD")
-            .setGclid(gclid);
+            .setCurrencyCode("USD");
 
-    if (conversionCustomVariableId != null && conversionCustomVariableValue != null) {
-      // Sets the custom variable and value, if provided.
-      clickConversionBuilder.addCustomVariables(
-          CustomVariable.newBuilder()
-              .setConversionCustomVariable(
-                  ResourceNames.conversionCustomVariable(customerId, conversionCustomVariableId))
-              .setValue(conversionCustomVariableValue));
-    }
-
+    // Sets the order ID if provided.
     if (orderId != null) {
-      // Sets the order ID (unique transaction ID), if provided. An order ID is required in order to
-      // upload enhancements as shown in the UploadConversionEnhancement example.
       clickConversionBuilder.setOrderId(orderId);
     }
 
+    // Creates a SHA256 message digest for hashing user identifiers in a privacy-safe way, as
+    // described at https://support.google.com/google-ads/answer/9888656.
+    MessageDigest sha256Digest = MessageDigest.getInstance("SHA-256");
+
+    // Creates a user identifier using the hashed email address, using the normalize and hash method
+    // specifically for email addresses.
+    // If using a phone number, use the normalizeAndHash(String) method instead.
+    String hashedEmail = normalizeAndHashEmailAddress(sha256Digest, emailAddress);
+    UserIdentifier userIdentifier =
+        UserIdentifier.newBuilder()
+            // User identifiers for conversion enhancements MUST use first party data.
+            .setUserIdentifierSource(UserIdentifierSource.FIRST_PARTY)
+            .setHashedEmail(hashedEmail)
+            .build();
+
+    // Adds the user identifier to the conversion.
+    clickConversionBuilder.addUserIdentifiers(userIdentifier);
+
+    // Calls build to build the conversion.
     ClickConversion clickConversion = clickConversionBuilder.build();
+    // [END create_conversion]
 
     // Creates the conversion upload service client.
     try (ConversionUploadServiceClient conversionUploadServiceClient =
@@ -212,5 +207,50 @@ public class UploadOfflineConversion {
       }
     }
   }
-  // [END upload_offline_conversion]
+  // [END upload_conversion_with_identifiers]
+
+  /**
+   * Returns the result of normalizing and then hashing the string using the provided digest.
+   * Private customer data must be hashed during upload, as described at
+   * https://support.google.com/google-ads/answer/7474263.
+   *
+   * @param digest the digest to use to hash the normalized string.
+   * @param s the string to normalize and hash.
+   */
+  // [START normalize_and_hash]
+  private String normalizeAndHash(MessageDigest digest, String s)
+      throws UnsupportedEncodingException {
+    // Normalizes by removing leading and trailing whitespace and converting all characters to
+    // lower case.
+    String normalized = s.trim().toLowerCase();
+    // Hashes the normalized string using the hashing algorithm.
+    byte[] hash = digest.digest(normalized.getBytes("UTF-8"));
+    StringBuilder result = new StringBuilder();
+    for (byte b : hash) {
+      result.append(String.format("%02x", b));
+    }
+
+    return result.toString();
+  }
+
+  /**
+   * Returns the result of normalizing and hashing an email address. For this use case, Google Ads
+   * requires removal of any '.' characters preceding {@code gmail.com} or {@code googlemail.com}.
+   *
+   * @param digest the digest to use to hash the normalized string.
+   * @param emailAddress the email address to normalize and hash.
+   */
+  private String normalizeAndHashEmailAddress(MessageDigest digest, String emailAddress)
+      throws UnsupportedEncodingException {
+    String normalizedEmail = emailAddress.toLowerCase();
+    String[] emailParts = normalizedEmail.split("@");
+    if (emailParts.length > 1 && emailParts[1].matches("^(gmail|googlemail)\\.com\\s*")) {
+      // Removes any '.' characters from the portion of the email address before the domain if the
+      // domain is gmail.com or googlemail.com.
+      emailParts[0] = emailParts[0].replaceAll("\\.", "");
+      normalizedEmail = String.format("%s@%s", emailParts[0], emailParts[1]);
+    }
+    return normalizeAndHash(digest, normalizedEmail);
+  }
+  // [END normalize_and_hash]
 }
