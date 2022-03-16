b5cf0ead97ff406bb676f80ac7a35b300d5d89a7
Josh Radcliff <jradcliff@users.noreply.github.com>
2022-03-16 11:22:19 -0400

    Update AddCampaigns example to illustrate Display Expansion setting

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/basicoperations/AddCampaigns.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/basicoperations/AddCampaigns.java
@@ -145,7 +145,9 @@ public class AddCampaigns {
           NetworkSettings.newBuilder()
               .setTargetGoogleSearch(true)
               .setTargetSearchNetwork(true)
-              .setTargetContentNetwork(false)
+              // Enable Display Expansion on Search campaigns. See
+              // https://support.google.com/google-ads/answer/7193800 to learn more.
+              .setTargetContentNetwork(true)
               .setTargetPartnerSearchNetwork(false)
               .build();
 
